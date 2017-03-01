5f71f58ba50c9535ba56c3acde0e6b1a6ebe4ade
Laura Moore <rileyle@gmail.com>
2017-02-28 23:18:36 -0500

    Tokens shown on post request, not shown on get request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      render json: @user, serializer: UserApiTokenSerializer
     else
       render json: @user.errors.full_messages, status: 400
     end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :email, :posts_count
 end
+
+class UserApiTokenSerializer < ActiveModel::Serializer
+  attributes :username, :email, :posts_count, :api_token
+end
